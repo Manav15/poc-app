183e81564522c9a969e1d4fcfd75aaa084faa75f
Manav Chug <Manav.Chug@assetmark.com>
2021-10-19 03:00:13 -0700

    Nav added

--- src/Components/Content/content.component.tsx
+++ src/Components/Content/content.component.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import CollapsibleTable from './TabContent';
-import styles from './Content.module.css'
+import CustomSeparator from '../NavBar/navBar.component';
+import styles from './Content.module.css';
+import { Box, Container, Grid } from '@material-ui/core';
 
 
 export const ContentComponent = () => {
+    const { tables, contentContainer } = styles
     return (
-        <div>
-            
-            <div className={styles.Tables}>
-                {CollapsibleTable()}
-            </div>
-        </div>
+        <Box>
+            <Grid container spacing={2}>
+                <Grid item>
+                    <h3>Net Flows</h3>
+                </Grid>
+                <Grid item>
+                    {CustomSeparator()}
+                </Grid>
+                <Grid item xs={12} >
+                    {CollapsibleTable()}
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
