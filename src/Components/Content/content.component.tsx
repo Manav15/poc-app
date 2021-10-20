2d3fc7b320a81e0b128826ddfec418c925cbc948
Manav Chug <Manav.Chug@assetmark.com>
2021-10-20 06:42:56 -0700

    Select box added

--- src/Components/Content/content.component.tsx
+++ src/Components/Content/content.component.tsx
@@ -3,20 +3,24 @@ import CollapsibleTable from './TabContent';
 import styles from './Content.module.css'
 import Chart from '../Chart/chart.component';
 import CustomSeparator from '../NavBar/navBar.component';
-
 import { Box, Container, Grid } from '@material-ui/core';
-
+import NativeSelectDemo from './selectBox.component';
 
 export const ContentComponent = () => {
     const { tables, contentContainer, netFlows } = styles
     return (
         <Box className={contentContainer}>
             <Grid container spacing={2}>
-                <Grid item>
-                    <p className={netFlows}>Net Flows</p>              
-                <h3>{CustomSeparator()}</h3>
+                <Grid item xs={1}>
                 </Grid>
-                <Grid item xs ={12}  >
+                <Grid item xs={7}>
+                    <p className={netFlows}>Net Flows</p>
+                    <h3>{CustomSeparator()}</h3>
+                </Grid>
+                <Grid item xs={4}>
+                    {NativeSelectDemo()}
+                </Grid>
+                <Grid item xs={12}  >
                     <div className="container" >
                         <div className="card">
                             <div className="card-body">
@@ -27,11 +31,11 @@ export const ContentComponent = () => {
                     </div>
                 </Grid>
                 <Grid item xs={12}  >
-                <div className="card">
-                    <div className="card-body">
-                    {CollapsibleTable()}
-                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            {CollapsibleTable()}
                         </div>
+                    </div>
                 </Grid>
             </Grid>
         </Box>
